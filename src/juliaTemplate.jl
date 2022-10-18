4cdee4a0411ac8e003a70a6523c96f63589c2cf9
SinaLewis <sile6618@colorado.edu>
2022-10-18 15:46:28 -0600

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/juliaTemplate.jl
@@ -0,0 +1,5 @@
+module juliaTemplate
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using juliaTemplate
+using Test
+
+@testset "juliaTemplate.jl" begin
+    # Write your tests here.
+end
